1e593b7b6886414442a2d250ee54e1d30a949c3f
Harshit Singh <harshit.singh817775@gmail.com>
2025-09-21 00:17:08 +0530

    chore: intial setup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     let a = 1;
-    println!("Hello, world! {}", a);
+    println!("Hello, world! {} today's sum is {}", a, sum(1, 12));
+}
+
+fn sum (a: i32, b: i32) -> i32 {
+    return a+b
 }
