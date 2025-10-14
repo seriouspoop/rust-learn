8502fc483f35d25be3d7b5d047501731304ba663
Harshit Singh <harshit.singh817775@gmail.com>
2025-10-14 23:16:36 +0530

    feat: modularize chapters into lessions module

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,5 @@
-mod chapter1;
-mod chapter2;
-mod chapter3;
-mod chapter4;
-mod chapter5;
-mod chapter6;
-
+mod lessions;
 fn main() {
-    // Data types
-    chapter1::data_types::primitives();
-    chapter1::data_types::strings();
-    chapter1::data_types::arrays();
-    chapter1::data_types::tuples();
-    chapter1::data_types::vectors();
-
-    // Control Flow
-    chapter2::control_flow::conditionals();
-    chapter2::control_flow::loops();
-
-    // Functions
-    chapter3::functions::example_function();
-
-    // Ownership
-    chapter4::ownership::borrowing();
-    chapter4::ownership::moving();
-    chapter4::ownership::references();
-    chapter4::ownership::slice_referencing();
-
-    // Structs
-    chapter5::structs::sample_struct();
-    chapter5::structs::update_syntax();
-    chapter5::structs::tuple_struct();
-    chapter5::structs::unit_structs();
-    chapter5::structs::methods();
-
-    // Enums and Pattern Matching
-    chapter6::enums::ip_addr_example();
-    chapter6::enums::option_enum();
-    chapter6::matching::enum_patterns();
-    chapter6::matching::if_let();
-    chapter6::matching::let_else();
+    // Load all lessions
+    lessions::lessions();
 }
